38572400b3fc7f6ef245154eebadb6448ca1fd8d
Nick Howell <nlhowell12@gmail.com>
2018-05-23 15:08:56 -0400

    fixed conditional

--- src/App.js
+++ src/App.js
@@ -23,7 +23,8 @@ const Card = (props) => (
         <div className="card__description cardGroup__cardDescription">
             <div className={`icon fa ${props.icon} card__descriptionIcon`} />
             <div className="card__descriptionText">
-            {props.description && <p>{props.hint}</p>}
+            {props.description}
+            {props.hint && <p>{props.hint}</p>}
             </div>
         </div>
         <div className="card__price">{props.price}</div>
